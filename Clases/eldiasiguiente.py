fb5baf41ce6a74a38088d6b00de7697583f4d246
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-03-23 16:53:43 +0100

    creo 2 listas

--- Clases/eldiasiguiente.py
+++ Clases/eldiasiguiente.py
@@ -1,5 +1,14 @@
-ciudad = str(input("en que ciudad quieres que se produzca la tragedia?: "))
+ciudad = str(input("en que ciudad quieres que se produzca la tragedia(NewYork o LosAngeles)?: "))
+edificios = ["A" , "B" ,"C"]
+empleados = ["señor Martín","señor Salim","señor Xing"]
 class destruccion:
-    def __init__(self,ciudad):
+    def __init__(self,ciudad,edificios,empleados):
         self.ciudad = ciudad
+        self.edificios = edificios
+        self.empleados = empleados
+    def eleccion(self):
+        if self.ciudad == "NewYork":
+            
         
+           print
+        elif self.ciudad == "LosAngeles"
