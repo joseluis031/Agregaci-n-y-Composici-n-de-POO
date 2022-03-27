f127779954afdc384a37e174f2653ef887dd7a58
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-03-27 14:09:59 +0200

    falta la destruccion

--- Clases/eldia2.py
+++ Clases/eldia2.py
@@ -6,9 +6,11 @@ class NuevaYork:
         self.edificios = edificios
         self.persona = persona
     def Edificios(self):
-        self.edificios.extend = (["A","B"])
+        self.edificios.extend(["A","B"])
+        print("En NuevaYork se encuentran los edificios", self.edificios)
     def Persona(self):
-        self.persona.extend = (["señor Martín","señor Salim"])
+        self.persona.extend(["señor Martín","señor Salim"])
+        print("En NuevaYork se encuentran los empleados", self.persona)
 class LosAngeles:
     def __init__(self,ciudad,edificios,persona):
         self.ciudad = ciudad
@@ -16,22 +18,8 @@ class LosAngeles:
         self.persona = persona
     def Edificios(self):
         self.edificios.append("C")
-        print("En LosAngeles se encuentra el edificio C")
+        print("En LosAngeles se encuentra el edificio",self.edificios)
     def Persona(self):
         self.persona.append("señor Xing")
+        print("En LosAngeles se encuentra el empleado", self.persona)
 
-if __name__ == "__main__":  
-    ciudad = str(input("en que ciudad quieres que se produzca la tragedia(NewYork o LosAngeles)?: "))
-    if ciudad == "NewYork":
-        from Clases.eldia2 import NuevaYork
-        total = NuevaYork(ciudad, edificios, persona)
-        print(total.Edificios)
-        print(total.Persona)
-    elif ciudad == "LosAngeles":
-        from Clases.eldia2 import LosAngeles
-        total1 = LosAngeles(ciudad,edificios,persona)
-        print(total1.Edificios)
-        print(total1.Persona)
-    else:
-        print("Esa ciudad no esta disponible")
-        pass
