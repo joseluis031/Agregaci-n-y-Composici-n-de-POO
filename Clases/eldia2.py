07ddeb083c11032a23d8516f22333d8ee417b899
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-03-28 00:04:57 +0200

    creo que lo tengo

--- Clases/eldia2.py
+++ Clases/eldia2.py
@@ -5,23 +5,33 @@ class NuevaYork:
         self.ciudad = ciudad
         self.edificios = edificios
         self.persona = persona
+    def __del__(self):
+        print("Los edificios",self.edificios,"han sido derrumbados")
+        print("Los empleados",self.persona,"han muerto")
     def Edificios(self):
         self.edificios.extend(["A","B"])
-        print("En NuevaYork se encuentran los edificios", self.edificios)
+        print("En NuevaYork se encuentran los edificios")
+        return self.edificios
     def Persona(self):
         self.persona.extend(["señor Martín","señor Salim"])
-        print("En NuevaYork se encuentran los empleados", self.persona)
+        print("En NuevaYork se encuentran los empleados")
+        return self.persona
 class LosAngeles:
     def __init__(self,ciudad,edificios,persona):
         self.ciudad = ciudad
         self.edificios = edificios
         self.persona = persona
-    def Edificios(self):
-        self.edificios.append("C")
-        print("En LosAngeles se encuentra el edificio",self.edificios)
     def __del__(self):
         print("El edificio",self.edificios,"ha sido derrumbado")
+        print("El empleado",self.persona,"ha muerto")
+    def Edificios(self):
+        self.edificios.append("C")
+        print("En LosAngeles se encuentra el edificio")
+        return self.edificios
     def Persona(self):
         self.persona.append("señor Xing")
-        print("En LosAngeles se encuentra el empleado", self.persona)
+        print("En LosAngeles se encuentra el empleado")
+        return self.persona
+    
+    
 
--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 if __name__ == "__main__":  
-    ciudad = str(input("en que ciudad quieres que se produzca la tragedia(NewYork o LosAngeles)?: "))
+    ciudad = str(input("En que ciudad quieres que se produzca la tragedia(NewYork o LosAngeles)?: "))
     edificios = []
     persona = []
     
     if ciudad == "NewYork":
         from Clases.eldia2 import NuevaYork
-        def Create_obj(): 
-            print('Making Object...') 
-            obj = NuevaYork(ciudad,edificios,persona) 
-            print('function end...') 
-            return obj
-        obj = Create_obj()
-        del obj
-        
+        total = NuevaYork(ciudad, edificios, persona)
+        print(total.Edificios())
+        print(total.Persona())
+        print("Ha habido fuertes consecuencias...:")
+        del total
+        #print(total.Edificios()) ya me dice que no esta definido porque he utilizado el destructor
     elif ciudad == "LosAngeles":
         from Clases.eldia2 import LosAngeles
         total1 = LosAngeles(ciudad,edificios,persona)
         print(total1.Edificios())
         print(total1.Persona())
+        print("Ha habido fuertes consecuencias...:")
+        del total1
+        #print(total1.Edificios()) ya me dice que no esta definido porque he utilizado el destructor
     else:
         print("Esa ciudad no esta disponible")
         pass
