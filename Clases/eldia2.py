a180db891cba5cf05af9c7bcc6ec2920813fb7e1
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-03-27 13:48:54 +0200

    commit

--- Clases/eldia2.py
+++ Clases/eldia2.py
@@ -1,33 +1,34 @@
-
+edificios = []
+persona = []
 class NuevaYork:
     def __init__(self,ciudad,edificios,persona):
         self.ciudad = ciudad
         self.edificios = edificios
         self.persona = persona
     def Edificios(self):
-        self.edificios = ["A" , "B"]
+        self.edificios.extend = (["A","B"])
     def Persona(self):
-        self.persona = ["señor Martín","señor Salim"]
-        
+        self.persona.extend = (["señor Martín","señor Salim"])
 class LosAngeles:
-    def __init__(self,ciudad1,edificios1,persona1):
-        self.ciudad1 = ciudad1
-        self.edificios1 = edificios1
-        self.persona1 = persona1
+    def __init__(self,ciudad,edificios,persona):
+        self.ciudad = ciudad
+        self.edificios = edificios
+        self.persona = persona
     def Edificios(self):
-        self.edificios1 = "C"
+        self.edificios.append("C")
+        if ciudad == "LosAngeles":
+            print("En LosAngeles se encuentra el edificio C",self.edificios)
     def Persona(self):
-        self.persona1 = "señor Xing"
+        self.persona.append("señor Xing")
 
-
-if __name__ == "__main__":
+if __name__ == "__main__":  
     ciudad = str(input("en que ciudad quieres que se produzca la tragedia(NewYork o LosAngeles)?: "))
     if ciudad == "NewYork":
-        total = NuevaYork
+        total = NuevaYork(ciudad, edificios, persona)
         print(total.Edificios)
         print(total.Persona)
     elif ciudad == "LosAngeles":
-        total1 = LosAngeles
+        total1 = LosAngeles(ciudad,edificios,persona)
         print(total1.Edificios)
         print(total1.Persona)
     else:
--- Clases/eldiasiguiente.py
+++ Clases/eldiasiguiente.py
@@ -1,4 +1,3 @@
-ciudad = str(input("en que ciudad quieres que se produzca la tragedia(NewYork o LosAngeles)?: "))
 edificios = ["A" , "B" ,"C"]
 edificios_destruidos = []
 empleados = ["señor Martín","señor Salim","señor Xing"]
@@ -12,7 +11,7 @@ class destruccion:
         self.empleados_masacrados = empleados_masacrados
     def eleccion(self):
         if self.ciudad == "NewYork":
-            edificios_destruidos.extend([edificios[0],edificios[1]])
+            self.edificios_destruidos.extend(["A","B"])
             empleados_masacrados.extend([empleados[0],empleados[1]])
             print("Se ha destruido la ciudad de NewYork y las consecuencias son brutales!!!: ")
             print("Se han destruido los edificios",self.edificios_destruidos, "y se ha acabado con la vida de los empresarios ",self.empleados_masacrados)
@@ -22,5 +21,8 @@ class destruccion:
             print("Se ha destruido la ciudad de LosAngeles y las consecuencias son brutales!!!: ")
             print("Se han destruido los edificios",self.edificios_destruidos, "y se ha acabado con la vida de los empresarios ",self.empleados_masacrados)
             
-final = destruccion(ciudad,edificios,empleados,edificios_destruidos,empleados_masacrados)
-print(final.eleccion())
+
+if __name__ == "__main__":  
+    ciudad = str(input("en que ciudad quieres que se produzca la tragedia(NewYork o LosAngeles)?: "))
+    final = destruccion(ciudad,edificios,empleados,edificios_destruidos,empleados_masacrados)
+    print(final.eleccion())
