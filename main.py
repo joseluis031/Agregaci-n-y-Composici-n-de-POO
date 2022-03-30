a0f6038cdc0b6f82b508c49a6971133493499057
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-03-30 18:34:18 +0200

    actualizacion main

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 if __name__ == "__main__":  
-    ciudad = str(input("En que ciudad quieres que se produzca la tragedia(NewYork o LosAngeles)?: "))
-    edificios = []
-    persona = []
-    
-    if ciudad == "NewYork":
-        from Clases.eldia2 import NuevaYork
-        total = NuevaYork(ciudad, edificios, persona)
-        print(total.Edificios())
-        print(total.Persona())
-        print("Ha habido fuertes consecuencias...:")
-        del total
-        #print(total.Edificios()) ya me dice que no esta definido porque he utilizado el destructor
-    elif ciudad == "LosAngeles":
-        from Clases.eldia2 import LosAngeles
-        total1 = LosAngeles(ciudad,edificios,persona)
-        print(total1.Edificios())
-        print(total1.Persona())
-        print("Ha habido fuertes consecuencias...:")
-        del total1
-        #print(total1.Edificios()) ya me dice que no esta definido porque he utilizado el destructor
-    else:
-        print("Esa ciudad no esta disponible")
-        pass
+    main = int(input("Que ejercicio deseas realizar(1,2 o 3):"))
+    if main == 1:
+        ciudad = str(input("En que ciudad quieres que se produzca la tragedia(NewYork o LosAngeles)?: "))
+        edificios = []
+        persona = []
+        
+        if ciudad == "NewYork":
+            from Clases.eldia2 import NuevaYork
+            total = NuevaYork(ciudad, edificios, persona)
+            print(total.Edificios())
+            print(total.Persona())
+            print("Ha habido fuertes consecuencias...:")
+            del total
+            #print(total.Edificios()) ya me dice que no esta definido porque he utilizado el destructor
+        elif ciudad == "LosAngeles":
+            from Clases.eldia2 import LosAngeles
+            total1 = LosAngeles(ciudad,edificios,persona)
+            print(total1.Edificios())
+            print(total1.Persona())
+            print("Ha habido fuertes consecuencias...:")
+            del total1
+            #print(total1.Edificios()) ya me dice que no esta definido porque he utilizado el destructor
+        else:
+            print("Esa ciudad no esta disponible")
+            pass
+    elif main == 2:
+        from Clases.inmortal import *
+          
+        yin = Yin() 
+        yang = Yang() 
+        yin.yang = yang 
+        
+        print(yang)  
+        print(yang is yin.yang)  
+        del(yang) 
+        print("?")
+    elif main ==3:
+        from Clases.herencia import *
+        casa = Casa([pared_norte, pared_oeste, pared_sur, pared_cortina]) 
+        print(casa.superficie_acristalada())
